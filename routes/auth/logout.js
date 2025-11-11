39752d4324f2b58422fa5c13da9b0d86ff4b0779
Aditya Singh <1211821022@ngpp.ac.in>
2025-11-11 18:09:27 +0530

    adds logout route

---
+++ routes/auth/logout.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const Router = express.Router();
+
+Router.get("/logout", (req, res) => {
+    res.cookie("userLogged", "");
+    res.status(200).json({ message: `User Logged Out`, success: true })
+})
+
+module.exports = Router;
